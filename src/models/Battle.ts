f69cebe097adac1fdedf0434c0ee044a805fc542
Weslley <weslley_ss@live.com>
2024-06-18 22:01:24 -0300

    criando componentes de usuário

---
+++ src/models/Battle.ts
@@ -0,0 +1,11 @@
+import { Torneio } from "./Torneio";
+import { User } from "./User";
+
+export interface Battle{
+    battleId?: string;
+    userId?: string;
+    torneioId?: string;
+    jogada: string;
+    user: User;
+    torneio: Torneio;
+}
