0d666bdb2ddf29ab398948c8fab3d09e5ac5b873
Weslley <weslley_ss@live.com>
2024-06-23 17:50:17 -0300

    criando componente de listagem de usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import UserCadastrar from './components/pages/user/user-cadastrar';
+import UserCadastrar from "./components/pages/user/user-cadastrar";
+import UserListar from "./components/pages/user/user-listar";
 
 function App() {
   return (
@@ -9,16 +10,24 @@ function App() {
         <nav>
           <ul>
             <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
               <Link to="/users/cadastrar">Cadastrar Usuário</Link>
+            </li>
+            <li>
+              <Link to="/users/listar">Listar Usuários</Link>
             </li>
           </ul>
         </nav>
         <Routes>
+          <Route path="/" element={<UserListar />} />
           <Route path="/users/cadastrar" element={<UserCadastrar />} />
+          <Route path="/users/listar" element={<UserListar />} />
         </Routes>
       </BrowserRouter>
       <footer>
-          <p>Projeto Jokenpo &copy; 2024</p>
+        <p>Projeto Jokenpo &copy; 2024</p>
       </footer>
     </div>
   );
--- src/components/pages/user/user-listar.tsx
+++ src/components/pages/user/user-listar.tsx
@@ -1,4 +1,55 @@
-function UserListar(){
-    return;
+import { useState, useEffect } from "react";
+import { User } from "../../../models/User";
+import axios from "axios";
+
+function UserListar() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    carregarUsers();
+  }, []);
+
+  function carregarUsers() {
+    axios.get<User[]>("http://localhost:5154/users/listar").then((resposta) => {
+      setUsers(resposta.data);
+    });
+  }
+
+  return (
+    <div>
+      <h1>Lista de users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Telefone</th>
+            <th>Idade</th>
+            <th>Criado Em</th>
+            <th>Remover</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.nome}</td>
+              <td>{user.email}</td>
+              <td>{user.telefone}</td>
+              <td>{user.idade}</td>
+              <td>{user.criadoEm}</td>
+              {/* <td>
+                      <button type="button" onClick={() => { remover(user.id!) }}>Remover</button>
+                    </td>
+                    <td>
+                      <button type="button"><Link to={/user/editar/${user.id!}}>Alterar</Link></button>
+                    </td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default UserListar;
